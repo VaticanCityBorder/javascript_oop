0d612970184f56e832585013841477dc8026d03b
Maurer Viktor Endre <maurer.viktor@bolyai.local>
2026-01-30 12:43:30 +0100

    Órai munka

---
+++ syntax/functions.js
@@ -0,0 +1,12 @@
+export {muvelet, muveletLetrehoz};
+
+function muvelet(a, b, callback) {
+    const result = callback(a, b);
+    return {result};
+}
+
+function muveletLetrehoz(jel) {
+    if (jel == "+") {
+        return (egyik, masik) => egyik + masik;
+    }
+}
---
+++ syntax/index.js
@@ -0,0 +1,25 @@
+import { muvelet, muveletLetrehoz } from "./functions.js";
+
+const input1 = document.createElement("input");
+document.body.appendChild(input1);
+
+document.body.appendChild(document.createElement("br"));
+
+const input2 = document.createElement("input");
+document.body.appendChild(input2);
+
+const div = document.createElement("div");
+document.body.appendChild(div);
+
+const button = document.createElement("button");
+document.body.appendChild(button);
+button.innerText = "Összeadás";
+
+
+button.addEventListener("click", function() {
+    const num1 = Number(input1.value);
+    const num2 = Number(input2.value);
+
+    const eredmeny = muvelet(num1, num2, muveletLetrehoz("+"));
+    div.innerText = eredmeny.result;
+})
